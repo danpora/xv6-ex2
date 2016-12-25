a742545a985c5351c0b1336c9b359f98398168ae
danporat <einigo@gmail.com>
2016-12-25 22:17:31 +0200

    submission2

--- ulttest.d
+++
@@ -1,2 +0,0 @@
-ulttest.o: ulttest.c /usr/include/stdc-predef.h types.h stat.h user.h \
- uthread.h
--- uthread.d
+++
@@ -1,2 +0,0 @@
-uthread.o: uthread.c /usr/include/stdc-predef.h types.h user.h uthread.h \
- x86.h
